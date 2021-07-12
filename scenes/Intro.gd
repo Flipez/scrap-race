3a97e6daeba1ed8602c7d6094b869d506a85833b
Kjarrigan <github@kjarrigan.de>
2021-07-12 22:30:33 +0200

    Make the Intro work

---
+++ scenes/Intro.gd
@@ -0,0 +1,56 @@
+extends Node
+
+var cooldown = Timer.new()
+var dialog_pos = -1
+var dialog = [
+    ['???', 'Scrat...'],
+    ['???', 'Scrat!'],
+    ['???', 'SCRAT!!!'],
+    # Clonk
+    ['Scrat', 'Huh?'],
+    ['???', 'WAKE UP!!!'],
+    ['Scrat', "Why? It's like...oh...well...it's too early too stand up."],
+    ['???', "We're circling around the planet for two days now and you still haven't even selected a landing spot."],
+    ['???', "We're way behind schedule. You know that you'll loose your treasure hunter licence if you don't..."],
+    ['Scrat', "Ok, ok mom. I got it. Wake me up tomorrow and we'll dig up some \"treasure\" on this junkyard for the boss."],
+    ['M.O.M.', "I'm not your mom. I'm an artificial hyper intelligent advisor..but speaking of her. " ],
+    ['M.O.M.', 'I could...'],
+    ['Scrat', "Don't you dare..."],
+    ['Scrat', "Ok, ok, I'll stand up..."]
+]
+
+func _ready():
+    # $Player/Dialog.hide()
+    cooldown.one_shot = true
+    cooldown.connect("timeout", self, "_on_timer_timeout")
+    add_child(cooldown)
+
+func _input(event):
+    print(cooldown.time_left)
+    if cooldown.time_left > 0:
+        return 
+
+    if InputMap.event_is_action(event, 'ui_interact'):
+        $Player/Dialog/Arrow.hide()
+        dialog_pos += 1
+
+        # Call Mom        
+        if dialog_pos == 10:
+            $Telephone.play()
+            
+        if dialog_pos == 12:
+            $Telephone.stop()
+        
+        if dialog_pos >= dialog.size():
+            $Player/Dialog.hide()
+            get_tree().change_scene("res://Spaceship.tscn")
+            return
+        
+        var msg = dialog[dialog_pos]       
+        $Player/Dialog/Box/Speaker.text = msg[0]
+        $Player/Dialog/Box/Message.text = msg[1]
+        $Player/Dialog.show()
+        cooldown.start(0.5)
+
+func _on_timer_timeout():
+    $Player/Dialog/Arrow.show()
--- scenes/SpaceShip.gd
+++
@@ -1,7 +0,0 @@
-extends Sprite
-
-func _ready():
-  pass # Replace with function body.
-
-func _process(delta):
-  position.x -= 1
